0fb9aa23ea0d4fe87ca8ab29fc415b6f9fdfd904
Ahmed <ahmedqahmed46420@gmail.com>
2022-10-28 15:12:57 +0200

    Adding 'memoryCache' option to save data even after changing the languae

--- multilang.js
+++ multilang.js
@@ -2,44 +2,66 @@ const fs = require('fs'),
     LangNotSet = require("./errors/LangNotSet");
 
 class Multilang {
+    #lang = '';
+    #langDir = '';
+    #files = [];
+    #data = {};
+    #memoryCache = false;
+
     constructor() {
         this.reset();
     }
 
     reset() {
-        this.lang = '';
-        this.files = [];
-        this.data = {};
+        this.#lang = '';
+        if (!this.#memoryCache) {
+            this.#files = [];
+            this.#data = {};
+        }
+    }
+
+    setMemoryCache(memoryCache) {
+        this.#memoryCache = memoryCache;
+        return this;
     }
 
     setLang(langDir) {
         this.reset();
-        this.langDir = langDir;
-        this.lang = langDir.split('/').pop();
+        this.#langDir = langDir;
+        this.#lang = langDir.split('/').pop();
         return this;
     }
 
     getLang() {
-        return this.lang;
-    }
-
-    validate() {
-        if (this.langDir == '') {
-            throw new LangNotSet("You must set language using setLang() method");
-        }
+        return this.#lang;
     }
 
     get(keyPath) {
-        this.validate();
+        this.#validate();
+
+        const key = this.#getKey(keyPath);
+        const keyFullPath = `${ this.#langDir.replaceAll("/", ".") }.${keyPath}`;
+        const filePath = `${keyFullPath.replace('.' + key, '').replaceAll('.', '/')}.json`;
+
+        if ( this.#files.indexOf(filePath) === -1 && fs.existsSync(filePath) ) this.#addFileData(filePath);
+
+        return this.#data[keyFullPath] || key;
+    }
+
+    #validate() {
+        if (this.#langDir == '') throw new LangNotSet("You must set language using setLang() method");
+    }
+
+    #getKey(keyPath) {
         let pathArr = keyPath.split('.');
-        const key = pathArr.pop();
-        const filePath = this.langDir + '/' + keyPath.replace('.' + key, '').replace('.', '/') + '.json';
-        if ( this.files.indexOf(filePath) == -1 && fs.existsSync(filePath) ){
-            const newData = JSON.parse( fs.readFileSync(filePath, 'utf8') );
-            this.data = Object.assign(this.data, newData);
-            this.files.push(filePath);
-        }
-        return this.data[key] || key;
+        return pathArr.pop();
+    }
+
+    #addFileData(filePath) {
+        const newData = JSON.parse( fs.readFileSync(filePath, 'utf8') );
+        const fileKey = filePath.slice(0, -5).replaceAll('/', '.');
+        for (let newKey in newData) this.#data[ `${fileKey}.${newKey}` ] = newData[newKey];
+        this.#files.push(filePath);
     }
 }
 
